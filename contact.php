16a474f34584d264415fa125052bc2503b57fcd6
Nikola Milun <nikola.milun2508@gmail.com>
2022-07-20 18:35:04 +0200

    Frontend changes
    Almost finished home and aboutme, started cotnact. Added more CSS animations and changed homepage style

---
+++ contact.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Source+Code+Pro&display=swap" rel="stylesheet">
+        <!-- Local css -->
+        <link rel="stylesheet" href="style.css">
+        <title>Contact me - Nikola Milun</title>
+    </head>
+    <body class="contactBody">
+        <header class="transparent pacificoFont">
+            <div class="logoHolder">
+                <!-- Logo image -->
+                <img src="" alt="Site Logo" class="logo">
+            </div>
+            <div class="headerLinksHolder">
+                <!-- Navbar links -->
+                <a href="index.html" class="headerLink hlHome">Home</a>
+                <a href="about.html" class="headerLink hlAbout">About</a>
+                <a href="gallery.html" class="headerLink hlGallery">Gallery</a>
+                <a href="#" class="headerLink hlContact">Contact</a>
+            </div>
+        </header>
+    </body>
+</html>
