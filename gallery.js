51f774e53ccd6a8e6aeedbdef24929fc9a663a9e
Nikola Milun <nikola.milun2508@gmail.com>
2022-07-26 16:37:01 +0200

    Gallery improvs
    Added some gallery items and started the JS process.

---
+++ gallery.js
@@ -0,0 +1,11 @@
+const images = document.querySelector('.galleryPic');
+const shadow = document.querySelector('.shadow');
+
+let displayBigImage = function(image) {
+    shadow.style.opacity = '0.75';
+    
+}
+
+images.forEach(element => {
+    element.addEventListener('click', )
+});
